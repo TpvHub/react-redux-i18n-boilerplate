4fdfe4953c36babbcd4ac0c5298265938fcb4c92
Tinh Phan <tinh.phan.v@gmail.com>
2019-11-02 22:33:43 +0700

    Update AuthRoute component

--- src/elements/AuthRoute/AuthRoute.js
+++ src/elements/AuthRoute/AuthRoute.js
@@ -1,15 +1,16 @@
-import React from 'react'
-import { connect } from 'react-redux'
-import { Route, Redirect } from 'react-router-dom'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
 
 class AuthRoute extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       isAllowEnter: false,
       urlRedirect: null,
-    }
+    };
   }
 
   componentDidMount() {
@@ -17,29 +18,52 @@ class AuthRoute extends React.Component {
       .then(() => {
         this.setState({
           isAllowEnter: true
-        })
+        });
       })
       .catch(urlRedirect => {
         this.setState({
           urlRedirect,
-        })
-      })
+        });
+      });
   }
 
   render() {
-    const { path, component: Component, ...rest } = this.props
+    // extract props and state
+    const { path, component: Component, ...rest } = this.props;
+    const { urlRedirect, isAllowEnter } = this.state;
+
     return (
-      <Route path={path} render={(props) => {
-        if (this.state.isAllowEnter) {
-          return <Component {...props} {...rest} />
-        }
-        if (this.state.urlRedirect === null) return null
-        return <Redirect to={this.state.urlRedirect} />
-      }} />
-    )
+      <Route
+        path={path}
+        render={(props) => {
+          if (isAllowEnter) {
+            return <Component {...props} {...rest} />;
+          }
+          if (urlRedirect === null) return null;
+          return <Redirect to={urlRedirect} />;
+        }}
+      />
+    );
   }
 }
 
-export default connect((state => ({
-  store: state
-})))(AuthRoute)
+AuthRoute.propTypes = {
+  middlewares: PropTypes.array,
+  store: PropTypes.object,
+  path: PropTypes.string.isRequired,
+  component: PropTypes.elementType,
+};
+
+AuthRoute.defaultProps = {
+  middlewares: [],
+  store: {},
+  component: <div />
+};
+
+const mapStateToProps = state => ({
+  store: state,
+});
+
+const mapDispatchToProps = {};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthRoute);
